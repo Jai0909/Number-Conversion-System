2b4ed44acec029dc4ab8377b5a11c0b8db25acfc
Jai <73595570+Jai0909@users.noreply.github.com>
2022-11-08 18:26:27 +0530

    Delete coderunner.py

--- coderunner.py
+++
@@ -1,5 +0,0 @@
-from ncs import *
-s = str(input("ENTER TEXT TO BE CONVERTED: "))
-a = int(input("ENTER BASE TO BE CONVERTED FROM: "))
-b = int(input("ENTER BASE TO BE CONVERTED TO: "))
-convertBase(s,a,b)
